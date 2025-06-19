0710ee5d89bfc29cd24ef8db03b59ad83ac7c429
Dung246 <dungoo2605@gmail.com>
2025-06-19 19:48:27 +0700

    Update PTIT_CNTT3_IT201_Session02_Bai04.c

--- PTIT_CNTT3_IT201_Session02_Bai04.c
+++ PTIT_CNTT3_IT201_Session02_Bai04.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int n, arr[100];
+    int n, *arr;
     int position, value;
     do {
         printf("Nhap so phan tu cua mang: ");
         scanf("%d", &n);
     } while (n <= 0 || n > 100);
+    arr = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         printf("Nhap arr[%d]: ", i);
         scanf("%d", &arr[i]);
     }
     do {
-        printf("Nhap vi tri muon sua (0 <= vitri < %d): ", n);
+        printf("Nhap vi tri muon sua: ", n);
         scanf("%d", &position);
     } while (position < 0 || position >= n);
+
     printf("Nhap gia tri moi: ");
     scanf("%d", &value);
     arr[position] = value;
@@ -23,6 +26,6 @@ int main() {
         printf("%d ", arr[i]);
     }
     printf("\n");
-
+    free(arr);
     return 0;
 }
